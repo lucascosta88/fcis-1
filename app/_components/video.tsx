021af3fbf635d29d63e6802bb606c2539ba25e0a
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-09-11 00:06:10 -0300

    feat: added page1 to code, modified the globalscss the button and the colors

--- app/_components/video.tsx
+++ app/_components/video.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 const Video = () => {
   return (
-    <div className='flex p-52 justify-center'>
-      <iframe
-        width="600"
+    <div className='flex py-2 justify-center'>
+      <iframe className='rounded-xl' 
+      style={{ boxShadow: '6px 6px 0px 0px rgba(233, 153, 12, 1)' }}
+        width="700"
         height="400"
-        src="https://www.youtube.com/embed/8PO6_ljVsjo"
+        src="https://www.youtube.com/embed/HehOc4jzHXo"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
--- app/_pages/page1.tsx
+++ app/_pages/page1.tsx
@@ -1,18 +1,53 @@
+import { Button } from "@/components/ui/button";
 import Video from "../_components/video";
+import React from "react";
 
 const Page1 = () => {
-  return (
-    <div
-      style={{
-        backgroundImage: "url('/01/BACKGROUND.png')", // Correct path to the image
-        height: '1000px',
-        backgroundSize: 'cover',  
-        backgroundPosition: 'center' 
-      }}
-    >
-      <Video />
-    </div>
-  );
-}
-
-export default Page1;
+    return (
+      <div
+        style={{
+          backgroundImage: "url('/01/BACKGROUND.png')", // Correct path to the image
+          height: '1000px',
+          backgroundSize: 'cover',  
+          backgroundPosition: 'center' 
+        }}
+      >
+        {/* Additional image */}
+        <img
+          src="/01/LOGO.png" // Path to the additional image
+          alt="Logo"
+          style={{
+            position: 'absolute',
+            top: '20px', // Adjust as needed
+            left: '50%',
+            transform: 'translateX(-50%)',
+            zIndex: 1
+          }}
+        />
+        {/* Content div */}
+        <div style={{ position: 'relative', zIndex: 2 }}>
+          <h1 className="text-amber-500 text-center"
+          style={{
+            fontSize: '48px',
+            fontWeight: 'bold',
+            padding: '150px 0',
+            paddingBottom: '20px',
+            lineHeight: '1.1',
+            textShadow: '2px 2px 4px rgba(0, 0, 0, 0)',
+          }}>
+            Desperta a tua vida extraordinária
+            <br />
+            24 a 27 de outubro em Lisboa
+          </h1>
+          <Video />
+            <div className="flex justify-center py-6">
+            <Button className="rounded-xl justify-center" variant={"goldFCS"}>
+                GARANTIR MINHA VAGA
+            </Button>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Page1;
