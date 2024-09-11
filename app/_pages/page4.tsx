4205cc7d64ca156a874c2d0d1155107fcbb3e2e8
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-09-11 01:32:24 -0300

    feat: added page4

---
+++ app/_pages/page4.tsx
@@ -0,0 +1,14 @@
+const Page4 = () => {
+    return ( 
+        <div style={{
+            backgroundImage: "url('/04/BLOCO.png')", // Correct path to the image
+            height: '500px',
+            backgroundSize: 'cover',  
+            backgroundPosition: 'center'
+        }}>
+
+        </div>
+     );
+}
+ 
+export default Page4;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Page1 from './_pages/page1';
 import Page2 from './_pages/page2';
 import Page3 from './_pages/page3';
+import Page4 from './_pages/page4';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Page1 />
       <Page2 />
       <Page3 />
+      <Page4 />
     </>
   );
 }
