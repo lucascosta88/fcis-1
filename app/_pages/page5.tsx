7ec212e64aafce5f81d2e06d4124f4702c0eb3e4
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-09-12 19:00:42 -0300

    fix: border page5

--- app/_pages/page5.tsx
+++ app/_pages/page5.tsx
@@ -25,12 +25,9 @@ const Page5 = () => {
                     backgroundColor: 'rgba(0, 0, 200, 0.1)', // Blue color with 50% opacity
                     padding: '25px',
                     paddingTop: '50px',
-                    paddingBottom: '50px',
-                    borderRadius: '8px', 
-                    
-                    //FIX TO COMMIT:
-                    // borderTopRightRadius: '32px',
-                    // borderBottomRightRadius: '32px'
+                    paddingBottom: '50px',               
+                    borderTopRightRadius: '32px',
+                    borderBottomRightRadius: '32px'
                   }}
                 >
                     <p className="text-xl font-bold px-16">
