d7df4aabfb9a58ebce26faa0e805afe5a3be2d5c
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-09-14 10:12:04 -0300

    feat: Started mobile construction creating the if tag to idenditify if is a mobile or not and created the folder and first page of mobile devices

---
+++ app/_pages/Mobile/page1Mobile.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Page1Mobile = () => {
+  return (
+    <div
+        style={{
+          backgroundImage: "url('/01/BACKGROUND.png')", // Correct path to the image
+          height: '910px',
+          backgroundSize: 'cover',  
+          backgroundPosition: 'center' 
+        }}>
+    </div>
+  );
+};
+
+export default Page1Mobile;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react';
 import Page1 from './_pages/page1';
 import Page2 from './_pages/page2';
@@ -9,20 +11,34 @@ import Page7 from './_pages/page7';
 import Page8 from './_pages/page8';
 import Page9 from './_pages/page9';
 import Page10 from './_pages/Page10';
+import useWindowSize from './_pages/Mobile/mobileIdentifier';
+import Page1Mobile from './_pages/Mobile/page1Mobile';
+
 
 export default function Home() {
+  const size = useWindowSize();
+  const isMobile = size.width <= 768;
+
   return (
     <>
-      <Page1 />
-      <Page2 />
-      <Page3 />
-      <Page4 />
-      <Page5 />
-      <Page6 />
-      <Page7 />
-      <Page8 />
-      <Page9 />
-      <Page10 />
+      {isMobile ? (
+        <>
+          <Page1Mobile />
+        </>
+      ) : (
+        <>
+          <Page1 />
+          <Page2 />
+          <Page3 />
+          <Page4 />
+          <Page5 />
+          <Page6 />
+          <Page7 />
+          <Page8 />
+          <Page9 />
+          <Page10 />
+        </>
+      )}
     </>
   );
 }
