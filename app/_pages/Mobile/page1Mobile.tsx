69c217b7a3c460556ab204fca5f651f8a4eb6360
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-09-14 10:43:18 -0300

    feat: added text, video, button and icon to page1mobile.tsx

--- app/_pages/Mobile/page1Mobile.tsx
+++ app/_pages/Mobile/page1Mobile.tsx
@@ -1,4 +1,6 @@
+import Video from '@/app/_components/video';
 import React from 'react';
+import Link from "@/components/ui/link";
 
 const Page1Mobile = () => {
   return (
@@ -9,6 +11,39 @@ const Page1Mobile = () => {
           backgroundSize: 'cover',  
           backgroundPosition: 'center' 
         }}>
+
+        <img
+          src="/01/LOGO.png" // Path to the additional image
+          alt="Logo"
+          style={{
+            position: 'absolute',
+            top: '20px', // Adjust as needed
+            left: '50%',
+            transform: 'translateX(-50%)',
+            zIndex: 1
+          }}
+        />
+        <div style={{ position: 'relative', zIndex: 2 }}>
+          <h1 className="text-amber-500 text-center"
+          style={{
+            fontSize: '48px',
+            fontWeight: 'bold',
+            fontFamily: 'Montserrat',
+            padding: '150px 0',
+            paddingBottom: '20px',
+            lineHeight: '1.1'
+          }}>
+            Desperta a tua vida extraordinária
+            <br />
+            24 a 27 de outubro em Lisboa
+          </h1>
+          <Video path="./videos/Método CIS.mp4" />
+            <div className="flex justify-center py-6">
+            <Link href="#Payments" className="rounded-xl justify-center" variant={"goldFCS"} size='lg'>
+            GARANTIR MINHA VAGA
+            </Link>
+            </div>
+        </div>
     </div>
   );
 };
