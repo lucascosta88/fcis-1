f0727577a8a2f34780fbca059d132b46c1a98a4c
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-09-16 20:49:06 -0300

    fix: deployed desktop aplication

--- app/_components/video.tsx
+++ app/_components/video.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import useWindowSize from '../_pages/Mobile/mobileIdentifier';
 
 interface VideoProps {
   path: string;
 }
 
 const Video: React.FC<VideoProps> = ({ path }) => {
-  const size = useWindowSize();
-  const isMobile = size.width <= 768;
-
-  const videoWidth = isMobile ? 300 : 700;
-  const videoHeight = isMobile ? 200 : 400;
-
   return (
     <div className='flex py-2 justify-center'>
       <iframe className='rounded-xl' 
       style={{ boxShadow: '6px 6px 0px 0px rgba(233, 153, 12, 1)' }}
-        width={videoWidth}
-        height={videoHeight}
+        width="700"
+        height="400"
         src={path}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
