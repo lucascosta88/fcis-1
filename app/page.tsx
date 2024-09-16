8ba84d6101c85c622e3fb451e5b4faf2a9e9aabe
Rafael "Caslu" Henry Cassiano Luiz <147253148+RCaslu@users.noreply.github.com>
2024-09-16 14:44:56 -0300

    Fix: fixed Page.tsx to create the upload file

--- app/page.tsx
+++ app/page.tsx
@@ -16,15 +16,16 @@ import Page10 from './_pages/Page10';
 export default function Home() {
   return (
      <>
-          <Page1 />
-          <Page2 />
-          <Page3 />
-          <Page4 />
-          <Page5 />
-          <Page6 />
-          <Page7 />
-          <Page8 />
-          <Page9 />
-          <Page10 />
-        </>
+        <Page1 />
+        <Page2 />
+        <Page3 />
+        <Page4 />
+        <Page5 />
+        <Page6 />
+        <Page7 />
+        <Page8 />
+        <Page9 />
+        <Page10 />
+    </>
+    );
 }
