866fe8ea853f3755a2184889c8fa2d89e19d9578
Lucas Araujo <lukas.scaa@gmail.com>
2024-09-18 21:43:46 +0100

    set domain febracis

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: 'https://febracislisboa.eu/fcis-react',
+    assetPrefix: process.env.NODE_ENV === 'development' 
+    ? ''  // Prefixo para developer
+    : 'https://meu-dominio.com',  // Prefixo para produção
 };
 
 export default nextConfig;
